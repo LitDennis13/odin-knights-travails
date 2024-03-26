6205161b6644002e1902e71ca9e717b7b33d3289
Dennis Antwi-Buadum <csrqwe13@gmail.com>
2024-03-26 16:43:43 -0600

    update GoodSolution

--- GoodSolution.js
+++ GoodSolution.js
@@ -1,10 +1,7 @@
 function checkArrayEquality(arr1,arr2) {
     return (arr1[0] === arr2[0] && arr1[1] === arr2[1])
 }
-/*
 
-
-*/
 function getPossibleKnightLocations(sq) {
     let arrayOfLocations = [];
     if (sq[0]-1 >= 0 && sq[1]-2 >= 0) {
